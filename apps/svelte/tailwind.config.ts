6d331b0dec454cae763b07bb435500b3f146ada7
JP Grusling <jp.grusling@fusionary.com>
2024-11-12 18:47:11 -0500

    format

--- apps/svelte/tailwind.config.ts
+++ apps/svelte/tailwind.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from 'tailwindcss'
 
-import baseConfig from '@local/tailwind-config';
+import baseConfig from '@local/tailwind-config'
 
 export default {
-	content: [
-		...baseConfig.content,
-		'../../packages/*/src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
-	],
-	presets: [baseConfig]
-} satisfies Config;
+  content: [
+    ...baseConfig.content,
+    '../../packages/*/src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+  presets: [baseConfig],
+} satisfies Config
