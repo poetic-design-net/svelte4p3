705f95e796218aa9fe3dcdc51fd12ad51bb0be5d
JP Grusling <jp.grusling@fusionary.com>
2024-11-12 09:25:23 -0500

    update svelte

--- apps/svelte/eslint.config.mjs
+++
@@ -1,5 +0,0 @@
-import { default as base } from '@local/eslint-config'
-
-const config = [...base]
-
-export default config
--- apps/svelte/vite.config.ts
+++ apps/svelte/vite.config.ts
@@ -1,27 +1,6 @@
-import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vite'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  resolve: {
-    conditions: [
-      'node',
-      'browser',
-      'module',
-      'jsnext:main',
-      'jsnext',
-      'svelte',
-    ],
-    mainFields: [
-      'node',
-      'browser',
-      'module',
-      'jsnext:main',
-      'jsnext',
-      'svelte',
-    ],
-  },
-  // ssr: {
-  //   noExternal: ['@payloadcms/ui'],
-  // },
-})
+	plugins: [sveltekit()]
+});
