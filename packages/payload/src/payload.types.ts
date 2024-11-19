34a8a01ef53333a0bd5d62d664920efa0de930a9
JP Grusling <jp.grusling@fusionary.com>
2024-11-18 20:50:14 -0500

    updated payload types

--- packages/payload/src/payload.types.ts
+++ packages/payload/src/payload.types.ts
@@ -46,9 +46,9 @@ export interface Config {
   user: User & {
     collection: 'users';
   };
-  jobs?: {
+  jobs: {
     tasks: unknown;
-    workflows?: unknown;
+    workflows: unknown;
   };
 }
 export interface UserAuthOperations {
